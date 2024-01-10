6e2087e6abebb60f32908c27bfe95db5f8a53a42
Boris Randebrock <brandebr@cbr11s1.42barcelona.com>
2024-01-10 19:17:00 +0100

    to be added and checked... just a few more functions.. tomorrow is another day

--- level_4/ft_list_remove_if/ft_list_remove_if.c
+++ level_4/ft_list_remove_if/ft_list_remove_if.c
@@ -3,40 +3,40 @@
 /*                                                        :::      ::::::::   */
 /*   ft_list_remove_if.c                                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: brandebr <brandebr@student.42barcelona.co  +#+  +:+       +#+        */
+/*   By: brandebr <brandebr@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/07 13:57:33 by brandebr          #+#    #+#             */
-/*   Updated: 2024/01/09 11:23:06 by brandebr         ###   ########.fr       */
+/*   Updated: 2024/01/10 19:15:51 by brandebr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdlib.h>
 #include "../ft_foreach/ft_list.h"
+#include <stdlib.h>
 
-void ft_list_remove_if(t_list **begin_list, void *data_ref, int (*cmp)())
+void	ft_list_remove_if(t_list **begin_list, void *data_ref, int (*cmp)())
 {
-		t_list	*aux;
-		t_list	*del;
-		t_list	*prev;
+	t_list	*aux;
+	t_list	*del;
+	t_list	*prev;
 
-		aux = *begin_list;
-		prev = NULL;
-		while (aux)
+	aux = *begin_list;
+	prev = NULL;
+	while (aux)
+	{
+		if ((cmp)(aux->data, data_ref) == 0)
 		{
-				if ((cmp)(aux->data, data_ref) == 0)
-				{
-						del = aux;
-						if (prev == 0)
-								*begin_list = aux->next;
-						else
-								prev->next = aux->next;
-						aux = aux->next;
-						free(del);
-				}
-				else
-				{
-						prev = aux;
-						aux = aux->next;
-				}
+			del = aux;
+			if (prev == NULL)
+				*begin_list = aux->next;
+			else
+				prev->next = aux->next;
+			aux = aux->next;
+			free(del);
 		}
+		else
+		{
+			prev = aux;
+			aux = aux->next;
+		}
+	}
 }
