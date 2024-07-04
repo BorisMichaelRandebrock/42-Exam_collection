7acd774dac2948adb2deae80130ad6c93647472e
Boris Randebrock <brandebr@student.42barcelona.com>
2024-07-04 18:12:08 +0200

    added shorter microshell version

--- exam_rank_4/microshell.c
+++ exam_rank_4/microshell.c
@@ -6,7 +6,7 @@
 /*   By: brandebr <brandebr@42barcelona.com>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/02 11:25:56 by brandebr          #+#    #+#             */
-/*   Updated: 2024/07/02 12:20:38 by brandebr         ###   ########.fr       */
+/*   Updated: 2024/07/04 18:10:53 by brandebr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -79,3 +79,69 @@ int	main(int argc, char **argv, char **envp)
 	}
 	return (status);
 }
+// alternative.. shorter version
+/*
+static int	er(char *s, char *argv)
+{
+	while (*s)
+		write(2, s++, 1);
+	if (argv)
+		while (*argv)
+			write(2, argv++, 1);
+	write(2, "\n", 1);
+	return (1);
+}
+
+static int	cd(char **argv, int i)
+{
+	if (i != 2)
+		return (er("error: cd: bad arguments", NULL));
+	else if (chdir(argv[1]) == -1)
+		return (er("error: cd: cannot change directory to ", argv[1]));
+	return (0);
+}
+
+static int	exe(char **argv, char **envp, int i)
+{
+
+	int		status;
+	int		fd[2];
+	int		pipes = (argv[i] && !strcmp(argv[i], "|"));
+
+	if (pipes && pipe(fd) == -1)
+		return (er("error: fatal", NULL));
+	int		pid = fork();
+	if (!pid)
+	{
+		argv[i] = 0;
+		if (pipes && (dup2(fd[1], 1) == -1 || close(fd[1]) == -1 || close(fd[0]) == -1))
+			return (er("error: fatal", NULL));
+		execve(*argv, argv, envp);
+		return (er("error: cannot execute ", *argv));
+	}
+	waitpid(pid, &status, 0);
+	if (pipes && (dup2(fd[0], 0) == -1 || close(fd[0]) == -1 || close(fd[1]) == -1))
+		return (er("error: fatal", NULL));
+	return (0);
+}
+
+int	main(int argc, char **argv, char **envp)
+{
+	(void)argc;
+
+	while (*argv && *(argv + 1))
+	{
+		argv++;
+		int		i = 0;
+		while (argv[i] && strcmp(argv[i], "|") && strcmp(argv[i], ";"))
+			i++;
+		if (!strcmp(*argv, "cd"))
+			cd(argv, i);
+		else if (i)
+			exe(argv, envp, i);
+		argv += i;
+	}
+	return (0);
+}
+*/
+
